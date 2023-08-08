a33347561bcb5861714c74ac6569b4114f222439
Shaili Patel <sp2826@njit.edu>
2023-08-08 00:29:30 -0400

    Added Mailchimp and other GDPR configurations

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css';
+import { logPageView } from '../lib/ga4';
 
 export default function Home() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [hasUserGivenConsent, setHasUserGivenConsent] = useState(false);
+  const [ga4Loaded, setGa4Loaded] = useState(false);
+
+  useEffect(() => {
+    // Load the user's consent from local storage.
+    const userConsent = localStorage.getItem('userGAConsent');
+    if (userConsent === 'true') {
+      setHasUserGivenConsent(true);
+    }
+
+    // Check if the Google Analytics script has loaded.
+    const ga4Script = document.querySelector(`script[src*="${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}"]`);
+    if (ga4Script) {
+      ga4Script.onload = () => {
+        setGa4Loaded(true);
+      };
+    }
+
+    return () => {
+      if (ga4Script) {
+        ga4Script.onload = null;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (hasUserGivenConsent && ga4Loaded) {
+      logPageView();
+    }
+  }, [hasUserGivenConsent, ga4Loaded]);
+
+  const handleConsent = () => {
+    localStorage.setItem('userGAConsent', 'true');
+    setHasUserGivenConsent(true);
+  };
+
+  const handleOptOut = () => {
+    localStorage.setItem('userGAConsent', 'false');
+    setHasUserGivenConsent(false);
+  };
 
   const subscribe = async (e) => {
     e.preventDefault();
@@ -32,6 +73,18 @@ export default function Home() {
 
   return (
     <div className={styles.homeContainer}>
+      {!hasUserGivenConsent ? (
+        <div className={styles.consentBanner}>
+          We use cookies for analytics. By continuing, you consent to this use.
+          <button onClick={handleConsent}>I Consent</button>
+        </div>
+      ) : (
+        <div className={styles.consentBanner}>
+          You have consented to analytics tracking. 
+          <button onClick={handleOptOut}>Opt-Out</button>
+        </div>
+      )}
+      
       <section className={styles.section}>
         <h1>Hey, this is Shaili Patel</h1>
         <p>Passionate developer, currently pursuing masters in Computer Science</p>
@@ -53,6 +106,13 @@ export default function Home() {
         </form>
         {message && <p>{message}</p>}
       </section>
+
+      <footer className={styles.footer}>
+  {/* Link to the privacy policy */}
+  <Link href="/privacy">Privacy Policy</Link>
+</footer>
+
     </div>
   );
 }
+
